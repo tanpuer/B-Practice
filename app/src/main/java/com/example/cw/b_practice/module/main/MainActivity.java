125dab2d6c9c9d4207f34e4452a6737857e079ab
templechen <templechen@qiyi.com>
2017-05-24 16:05:28 +0800

    after setting night mode, two fragments were added, don't know how to solve

--- app/src/main/java/com/example/cw/b_practice/module/main/MainActivity.java
+++ app/src/main/java/com/example/cw/b_practice/module/main/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -118,8 +117,7 @@ public class MainActivity extends RxBaseActivity implements IMainContract.IMainV
         boolean isNight = PreferencesUtil.getBoolean(ConstantsUtil.SWITCH_MODE_KEY, false);
         if (isNight){
             head_switch_mode.setImageResource(R.drawable.ic_switch_night);
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            reCreateView();
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }else {
             head_switch_mode.setImageResource(R.drawable.ic_switch_daily);
         }
