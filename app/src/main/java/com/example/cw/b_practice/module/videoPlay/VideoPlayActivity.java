96fd991a5b5c3f26e4a97338ca1344e7392bee32
templechen <templechen@qiyi.com>
2017-06-09 18:44:08 +0800

    add ijkplayerview

--- app/src/main/java/com/example/cw/b_practice/module/videoPlay/VideoPlayActivity.java
+++ app/src/main/java/com/example/cw/b_practice/module/videoPlay/VideoPlayActivity.java
@@ -1,8 +1,11 @@
 package com.example.cw.b_practice.module.videoPlay;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.cw.b_practice.base.RxBaseActivity;
+import com.example.cw.b_practice.util.ConstantsUtil;
 
 /**
  * Created by cw on 2017/6/5.
@@ -32,5 +35,13 @@ public class VideoPlayActivity extends RxBaseActivity implements IVidePlayContra
     @Override
     public void setPresenter(IVidePlayContract.IVideoPlayPresenter presenter) {
         mPresenter = presenter;
+    }
+
+    public static void launch(Activity activity, int cid, String title) {
+
+        Intent mIntent = new Intent(activity, VideoPlayActivity.class);
+        mIntent.putExtra(ConstantsUtil.EXTRA_CID, cid);
+        mIntent.putExtra(ConstantsUtil.EXTRA_TITLE, title);
+        activity.startActivity(mIntent);
     }
 }
